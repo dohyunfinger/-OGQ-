a339e147d9665d93488093428a9e55baafa120fa
dohyunfinger <dohyun2937@gmail.com>
2026-07-22 19:30:50 +0900

    title 변경

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from frontend.main_page import render_main_page
 
 def main() -> None:
     st.set_page_config(
-        page_title="SilverLens 우승 예 동의합니다",
+        page_title="작품명:SilverLens 팀명:우승 예 동의합니다",
         page_icon="✅",
         layout="wide",
     )
