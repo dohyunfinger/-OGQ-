eaf36499d8014c3ac1e6e7593773d8b1e8b9c56c
c_so0hyuk37 <cshad0700379@gmail.com>
2026-07-23 18:19:59 +0900

    백엔드 프론트 연결

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
-import streamlit as st
-
-from frontend.main_page import render_main_page
-
-
-def main() -> None:
-    st.set_page_config(
-        page_title="작품명: SilverLens 팀명:우승 예 동의합니다",
-        page_icon="✅",
-        layout="wide",
-    )
-
-    render_main_page()
+from frontend.router import run_app
 
 
 if __name__ == "__main__":
-    main()
-#작품명:SilverLens 팀명:우승 예 동의합니다
+    run_app()
